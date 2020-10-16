a911f1ee73599362cb142a9246e485b5b7d8ce8b
Jun Kajii <junk4jii@gmail.com>
2020-10-16 16:00:29 -0300

    🎉 Initial commit

---
+++ src/js/vibe.js
@@ -0,0 +1,10 @@
+const vibes = [
+  '...and you are awesome!',
+  '...have a wonderful day!',
+  "...and you've got this!",
+  '...and so is this puppy! 🐶',
+];
+
+var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+document.querySelector('.vibe').append(vibe);
